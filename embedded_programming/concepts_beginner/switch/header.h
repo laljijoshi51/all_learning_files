db07c77ba6919dac345c64961ad03c3be0a9db21
laljijoshi51 <117966076+laljijoshi51@users.noreply.github.com>
2023-08-07 12:19:57 +0530

    Add files via upload

---
+++ embedded_programming/concepts_beginner/switch/header.h
@@ -0,0 +1,21 @@
+
+typedef unsigned char u8;
+typedef signed char s8;
+typedef unsigned int u16;
+typedef signed int s16;
+typedef unsigned long int u32;
+typedef signed long int s32;
+
+#define PORT0 0
+#define PORT1 1
+#define PORT2 2
+#define PORT3 3
+
+#define SET 1
+#define CLEAR 0
+
+void delay_ms(u16 ms);
+/*******************************************/
+
+void io_write(u8 port,u8 pin,u8 val);
+u8 io_read(u8 port,u8 pin);
